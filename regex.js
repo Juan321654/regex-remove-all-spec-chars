95a4c0b410856d2be1066a4cb9abb91828811fb4
juan camacho <juanc@tiv.solutions>
2021-09-29 12:00:09 -0400

    added start with a number

--- regex.js
+++ regex.js
@@ -3,14 +3,20 @@ let words = [
   "(html )# body",
   "__unders  bla yttyt",
   "    very$ # rare (format)- + ; {}",
-  " who & is ^^^ writ#ing Stuf7 li(k) th@!is"
+  " who & is ^^^ writ#ing Stuf7 li(k) th@!is",
+  "2005 products something",
+  "2021 vents and fans"
 ];
 let arr = [];
 for (let word of words) {
-  let noSpecChar = word.replace(/[^a-zA-Z0-9]/g, " ");
+  let findNumAtStart = word.match(/^(\d+)/g)
+  let removedNumAtStart = word.replace(/^(\d+)/g, "")
+  let noSpecChar = removedNumAtStart.replace(/[^a-zA-Z0-9]/g, " ");
   let noExtraSpaces = noSpecChar.replace(/\s+/g, " ");
   let trimStartEndSpaces = noExtraSpaces.replace(/^\s+|\s+$/g, "");
-  arr.push(trimStartEndSpaces.replace(/ +/g, "_"));
+  let normalized = trimStartEndSpaces.replace(/ +/g, "_")
+  normalized += `_${findNumAtStart}`
+  arr.push(normalized);
 }
 console.log(arr);
 // [
@@ -19,4 +25,6 @@ console.log(arr);
 //   'unders_bla_yttyt',
 //   'very_rare_format',
 //   'who_is_writ_ing_Stuf7_li_k_th_is'
+//   'products_something_2005',
+//   'vents_and_fans_2021'
 // ]
